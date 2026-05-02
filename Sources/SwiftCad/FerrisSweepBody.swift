b4de5381ff54bf9d6196d00a1efd6d66a6772ea0
Jordan C <jordan.c4922@gmail.com>
2026-05-01 22:37:49 -0600

    Add Dimensions to FerrisSweep

--- Sources/SwiftCad/FerrisSweepBody.swift
+++ Sources/SwiftCad/FerrisSweepBody.swift
@@ -1,35 +1,50 @@
 import Cadova
 
-//               _________
-//         _____/ ┌─────┐ \________________
-//   _____/ ┌─────┤     ├─────┬─────┐┌|  |┐\
-//  / ┌─────┤     ├─────┤     |     ||    ||
-//  | |     ├─────┤     ├─────┼─────┤|    ||
-//  | ├─────┤     ├─────┤     |     |└────┘|
-//  | |     ├─────┤     ├─────┼─────┤┌────┐|
-//  | ├─────┤     ├─────┤     |     |└────┘|
-//  | |     ├─────┘     └────┬┴────┬┴────┐ |
-//  \ └─────┘                |     |     | |
-//   \                       └─────┴─────┘ /
-//    ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-// ┌─────┐
-// |     | = A cherry mx keycap+switch
-// └─────┘
+//             ________
+//        ____/ ┌────┐ \_____________
+//   ____/ ┌────┤    ├────┬────┐┌| |┐\
+//  / ┌────┤    ├────┤    |    ||   ||
+//  | |    ├────┤    ├────┼────┤|   ||
+//  | ├────┤    ├────┤    |    |└───┘|
+//  | |    ├────┤    ├────┼────┤┌───┐|
+//  | ├────┤    ├────┤    |    |└───┘|
+//  | |    ├────┘    └───┬┴───┬┴───┐ |
+//  \ └────┘             |    |    | |
+//   \                   └────┴────┘ /
+//    ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 // ┌────┐
-// └────┘ = A TRRS female connector
-//
-// ┌|  |┐
-// |    | = A microcontroller (I'm using a promicro, but RP2040-zero or others will work)
-// |    |
+// |    | = A cherry mx keycap+switch
 // └────┘
+// ┌───┐
+// └───┘ = A TRRS female connector
+//
+// ┌| |┐
+// |   | = A microcontroller (I'm using a promicro, but RP2040-zero or others will work)
+// |   |
+// └───┘
+
+extension FerrisSweep {
+    struct Dimensions {
+        let switchHoleSize: Double = 14.1
+        let spacingBetweenSwitchHole: Double = 5
+        let minThickness: Double = 1.6
+        let maxThickness: Double = 5.5
+        let bottomSupportHeight: Double = 2.0
+        
+        let outerSpacing: Double
+        let wallThickness: Double = 3
+        
+        init(microcontroller: MicrocontrollerDimensions, trrs: TrrsDimensions, fingers: FingerOffsets) {
+            self.outerSpacing = spacingBetweenSwitchHole / 2
+        }
+    }
+}
 
 struct FerrisSweep: Shape3D {
     private let microcontroller: MicrocontrollerDimensions
     private let trrs: TrrsDimensions
     private let fingers: FingerOffsets
-    private let switchHoleSize: Double = 14
-    private let spacingBetweenSwitchHole: Double = 5
-    private let outerPadding: Double = 8
+    private let dimensions: Dimensions
     
     private var extraColOffset: Double {
         fingers.pointer - 2
@@ -39,6 +54,7 @@ struct FerrisSweep: Shape3D {
         self.microcontroller = microcontrollerDimensions
         self.trrs = trrsDimensions
         self.fingers = fingerOffsets
+        self.dimensions = Dimensions(microcontroller: microcontrollerDimensions, trrs: trrsDimensions, fingers: fingerOffsets)
     }
     
     private var columnVerticalOffsets: [Double] {
@@ -59,21 +75,21 @@ struct FerrisSweep: Shape3D {
             columnVerticalOffsets.enumerated()
                 .map { (Double($0), $1) }
                 .map { (index, value) in
-                    switchColumn(topLeft: Vector2D((switchHoleSize + spacingBetweenSwitchHole) * index, value))
+                    switchColumn(topLeft: Vector2D((dimensions.switchHoleSize + dimensions.spacingBetweenSwitchHole) * index, value))
                 }
             
             switchShape(
                 topLeft: Vector2D(
-                    3.75 * (switchHoleSize + spacingBetweenSwitchHole),
-                    extraColOffset - 3 * (switchHoleSize + spacingBetweenSwitchHole)
+                    3.75 * (dimensions.switchHoleSize + dimensions.spacingBetweenSwitchHole),
+                    extraColOffset - 3 * (dimensions.switchHoleSize + dimensions.spacingBetweenSwitchHole)
                 )
             )
             .rotated(.degrees(-10), around: .center)
             
             switchShape(
                 topLeft: Vector2D(
-                    4.8 * (switchHoleSize + spacingBetweenSwitchHole),
-                    extraColOffset - 3 * (switchHoleSize + spacingBetweenSwitchHole) - 6
+                    4.8 * (dimensions.switchHoleSize + dimensions.spacingBetweenSwitchHole),
+                    extraColOffset - 3 * (dimensions.switchHoleSize + dimensions.spacingBetweenSwitchHole) - 6
                 )
             )
             .rotated(.degrees(-20), around: .center)
@@ -83,12 +99,12 @@ struct FerrisSweep: Shape3D {
     var outline: any Geometry2D {
         BezierPath(
             linesBetween: [
-                Vector2D(switchOffsetForRowAt(index: 0) - outerPadding, fingers.pinky + outerPadding),
-                Vector2D(switchOffsetForRowAt(index: 1) - outerPadding, fingers.ring + outerPadding),
-                Vector2D(switchOffsetForRowAt(index: 2) - outerPadding, fingers.middle + outerPadding),
-                Vector2D(switchOffsetForRowAt(index: 2) + switchHoleSize + outerPadding, fingers.middle + outerPadding),
-                Vector2D(switchOffsetForRowAt(index: 3) + switchHoleSize + outerPadding, fingers.pointer + outerPadding),
-                Vector2D(switchOffsetForRowAt(index: 4) + switchHoleSize + outerPadding, extraColOffset + outerPadding)
+                Vector2D(switchOffsetForRowAt(index: 0) - dimensions.outerSpacing, fingers.pinky + dimensions.outerSpacing),
+                Vector2D(switchOffsetForRowAt(index: 1) - dimensions.outerSpacing, fingers.ring + dimensions.outerSpacing),
+                Vector2D(switchOffsetForRowAt(index: 2) - dimensions.outerSpacing, fingers.middle + dimensions.outerSpacing),
+                Vector2D(switchOffsetForRowAt(index: 2) + dimensions.switchHoleSize + dimensions.outerSpacing, fingers.middle + dimensions.outerSpacing),
+                Vector2D(switchOffsetForRowAt(index: 3) + dimensions.switchHoleSize + dimensions.outerSpacing, fingers.pointer + dimensions.outerSpacing),
+                Vector2D(switchOffsetForRowAt(index: 4) + dimensions.switchHoleSize + dimensions.outerSpacing, extraColOffset + dimensions.outerSpacing)
             ] + []
         ).stroked(width: 1, style: .round)
     }
@@ -96,9 +112,9 @@ struct FerrisSweep: Shape3D {
     private func switchShape(topLeft: Vector2D) -> any Geometry2D {
         BezierPath2D(linesBetween: [
             topLeft,
-            Vector2D(x: topLeft.x + switchHoleSize, y: topLeft.y),
-            Vector2D(x: topLeft.x + switchHoleSize, y: topLeft.y - switchHoleSize),
-            Vector2D(x: topLeft.x, y: topLeft.y - switchHoleSize)
+            Vector2D(x: topLeft.x + dimensions.switchHoleSize, y: topLeft.y),
+            Vector2D(x: topLeft.x + dimensions.switchHoleSize, y: topLeft.y - dimensions.switchHoleSize),
+            Vector2D(x: topLeft.x, y: topLeft.y - dimensions.switchHoleSize)
         ])
         .filled()
     }
@@ -107,7 +123,7 @@ struct FerrisSweep: Shape3D {
         Union(
             ([0, 1, 2] as [Double])
                 .map {
-                    switchShape(topLeft: Vector2D(topLeft.x, topLeft.y - (switchHoleSize + spacingBetweenSwitchHole) * $0))
+                    switchShape(topLeft: Vector2D(topLeft.x, topLeft.y - (dimensions.switchHoleSize + dimensions.spacingBetweenSwitchHole) * $0))
                 }
         )
     }
@@ -115,20 +131,20 @@ struct FerrisSweep: Shape3D {
 
 extension FerrisSweep {
     /// 0-indexed, left-most coordinate
-    // →┌─────┐
-    //  |     |
-    //  └─────┘
+    // →┌────┐
+    //  |    |
+    //  └────┘
     private func switchOffsetForRowAt(index: Int) -> Double {
-        (switchHoleSize + spacingBetweenSwitchHole) * Double(index)
+        (dimensions.switchHoleSize + dimensions.spacingBetweenSwitchHole) * Double(index)
     }
     
     /// 0-indexed, top-most coordinate
     //  ↓
-    //  ┌─────┐
-    //  |     |
-    //  └─────┘
+    //  ┌────┐
+    //  |    |
+    //  └────┘
     private func switchOffsetForColumnAt(index: Int, startingOffset: Double) -> Double {
-        (switchHoleSize + spacingBetweenSwitchHole) * Double(index) + startingOffset
+        (dimensions.switchHoleSize + dimensions.spacingBetweenSwitchHole) * Double(index) + startingOffset
     }
     
     private func switchOffsetFor(row: Int, column: Int, startingVerticalOffset: Double) -> Vector2D {
