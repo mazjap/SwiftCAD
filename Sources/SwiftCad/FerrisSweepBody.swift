96d4bf3e66f493f17686f4116208691c6ac1b532
Jordan C <jordan.c4922@gmail.com>
2026-05-02 21:00:11 -0600

    Finish frame of ferris sweep

--- Sources/SwiftCad/FerrisSweepBody.swift
+++ Sources/SwiftCad/FerrisSweepBody.swift
@@ -29,13 +29,52 @@ extension FerrisSweep {
         let spacingBetweenSwitchHole: Double = 5
         let minThickness: Double = 1.6
         let maxThickness: Double = 5.5
-        let bottomSupportHeight: Double = 2.0
+        let bottomSupportHeight: Double = 2
+        let wallThickness: Double = 3
         
         let outerSpacing: Double
-        let wallThickness: Double = 3
+        
+        let pinkyMinX: Double,   pinkyMaxX: Double,   pinkyMinY: Double,   pinkyMaxY: Double
+        let ringMinX: Double,    ringMaxX: Double,    ringMinY: Double,    ringMaxY: Double
+        let middleMinX: Double,  middleMaxX: Double,  middleMinY: Double,  middleMaxY: Double
+        let pointerMinX: Double, pointerMaxX: Double, pointerMinY: Double, pointerMaxY: Double
+        let otherMinX: Double,   otherMaxX: Double,   otherMinY: Double,   otherMaxY: Double
+        
+        let columnVerticalOffsets: [Double]
+        let thumbKeysXOffset: Double
         
         init(microcontroller: MicrocontrollerDimensions, trrs: TrrsDimensions, fingers: FingerOffsets) {
             self.outerSpacing = spacingBetweenSwitchHole / 2
+            
+            let colHeight = 3 * switchHoleSize + 2 * spacingBetweenSwitchHole
+            
+            self.pinkyMinX = 0
+            self.pinkyMaxX = pinkyMinX + switchHoleSize
+            self.pinkyMinY = fingers.pinky
+            self.pinkyMaxY = pinkyMinY + colHeight
+            
+            self.ringMinX = pinkyMaxX + spacingBetweenSwitchHole
+            self.ringMaxX = ringMinX + switchHoleSize
+            self.ringMinY = fingers.ring
+            self.ringMaxY = ringMinY + colHeight
+            
+            self.middleMinX = ringMaxX + spacingBetweenSwitchHole
+            self.middleMaxX = middleMinX + switchHoleSize
+            self.middleMinY = fingers.middle
+            self.middleMaxY = middleMinY + colHeight
+            
+            self.pointerMinX = middleMaxX + spacingBetweenSwitchHole
+            self.pointerMaxX = pointerMinX + switchHoleSize
+            self.pointerMinY = fingers.pointer
+            self.pointerMaxY = pointerMinY + colHeight
+            
+            self.otherMinX = pointerMaxX + spacingBetweenSwitchHole
+            self.otherMaxX = otherMinX + switchHoleSize
+            self.otherMinY = fingers.pointer - 2
+            self.otherMaxY = otherMinY + colHeight
+            
+            self.columnVerticalOffsets = fingers.orderedOffsets + [otherMinY]
+            self.thumbKeysXOffset = 3.6 * (switchHoleSize + spacingBetweenSwitchHole)
         }
     }
 }
@@ -46,19 +85,11 @@ struct FerrisSweep: Shape3D {
     private let fingers: FingerOffsets
     private let dimensions: Dimensions
     
-    private var extraColOffset: Double {
-        fingers.pointer - 2
-    }
-    
     init(microcontrollerDimensions: MicrocontrollerDimensions, trrsDimensions: TrrsDimensions, fingerOffsets: FingerOffsets) {
         self.microcontroller = microcontrollerDimensions
         self.trrs = trrsDimensions
         self.fingers = fingerOffsets
         self.dimensions = Dimensions(microcontroller: microcontrollerDimensions, trrs: trrsDimensions, fingers: fingerOffsets)
-    }
-    
-    private var columnVerticalOffsets: [Double] {
-        [fingers.pinky, fingers.ring, fingers.middle, fingers.pointer, extraColOffset]
     }
     
     var body: any Geometry3D {
@@ -72,82 +103,52 @@ struct FerrisSweep: Shape3D {
     
     var switchHoles: any Geometry2D {
         Union {
-            columnVerticalOffsets.enumerated()
-                .map { (Double($0), $1) }
-                .map { (index, value) in
-                    switchColumn(topLeft: Vector2D((dimensions.switchHoleSize + dimensions.spacingBetweenSwitchHole) * index, value))
+            Stack(.x, spacing: dimensions.spacingBetweenSwitchHole) {
+                for offset in dimensions.columnVerticalOffsets {
+                    Stack(.y, spacing: dimensions.spacingBetweenSwitchHole) {
+                        for _ in 1...3 {
+                            Rectangle(dimensions.switchHoleSize)
+                        }
+                    }
+                    .translated(y: offset)
                 }
+            }
             
-            switchShape(
-                topLeft: Vector2D(
-                    3.75 * (dimensions.switchHoleSize + dimensions.spacingBetweenSwitchHole),
-                    extraColOffset - 3 * (dimensions.switchHoleSize + dimensions.spacingBetweenSwitchHole)
-                )
-            )
-            .rotated(.degrees(-10), around: .center)
+            Rectangle(dimensions.switchHoleSize)
+                .translated(x: dimensions.thumbKeysXOffset)
+                .rotated(.degrees(-10), around: .center)
             
-            switchShape(
-                topLeft: Vector2D(
-                    4.8 * (dimensions.switchHoleSize + dimensions.spacingBetweenSwitchHole),
-                    extraColOffset - 3 * (dimensions.switchHoleSize + dimensions.spacingBetweenSwitchHole) - 6
-                )
-            )
-            .rotated(.degrees(-20), around: .center)
+            Rectangle(dimensions.switchHoleSize)
+                .translated(x: dimensions.thumbKeysXOffset + dimensions.switchHoleSize + dimensions.spacingBetweenSwitchHole, y: -5)
+                .rotated(.degrees(-20), around: .center)
         }
     }
     
     var outline: any Geometry2D {
-        BezierPath(
-            linesBetween: [
-                Vector2D(switchOffsetForRowAt(index: 0) - dimensions.outerSpacing, fingers.pinky + dimensions.outerSpacing),
-                Vector2D(switchOffsetForRowAt(index: 1) - dimensions.outerSpacing, fingers.ring + dimensions.outerSpacing),
-                Vector2D(switchOffsetForRowAt(index: 2) - dimensions.outerSpacing, fingers.middle + dimensions.outerSpacing),
-                Vector2D(switchOffsetForRowAt(index: 2) + dimensions.switchHoleSize + dimensions.outerSpacing, fingers.middle + dimensions.outerSpacing),
-                Vector2D(switchOffsetForRowAt(index: 3) + dimensions.switchHoleSize + dimensions.outerSpacing, fingers.pointer + dimensions.outerSpacing),
-                Vector2D(switchOffsetForRowAt(index: 4) + dimensions.switchHoleSize + dimensions.outerSpacing, extraColOffset + dimensions.outerSpacing)
-            ] + []
-        ).stroked(width: 1, style: .round)
-    }
-    
-    private func switchShape(topLeft: Vector2D) -> any Geometry2D {
-        BezierPath2D(linesBetween: [
-            topLeft,
-            Vector2D(x: topLeft.x + dimensions.switchHoleSize, y: topLeft.y),
-            Vector2D(x: topLeft.x + dimensions.switchHoleSize, y: topLeft.y - dimensions.switchHoleSize),
-            Vector2D(x: topLeft.x, y: topLeft.y - dimensions.switchHoleSize)
+        let radius = dimensions.switchHoleSize / 2
+        let hypotenuse = (radius * radius * 2).squareRoot()
+        
+        
+        return BezierPath2D(linesBetween: [
+            Vector2D(dimensions.pinkyMinX, dimensions.pinkyMinY),
+            Vector2D(dimensions.pinkyMinX, dimensions.pinkyMaxY),
+            Vector2D(dimensions.ringMinX, dimensions.ringMaxY),
+            Vector2D(dimensions.middleMinX, dimensions.middleMaxY),
+            Vector2D(dimensions.middleMaxX, dimensions.middleMaxY),
+            Vector2D(dimensions.pointerMaxX, dimensions.pointerMaxY),
+            Vector2D(dimensions.otherMaxX, dimensions.otherMaxY),
+            // Microcontroller here
+            // Thank you precalc for teaching me trig haha 🙏
+            Vector2D(dimensions.thumbKeysXOffset + dimensions.switchHoleSize * 1.5 + dimensions.spacingBetweenSwitchHole + cos(.degrees(45 - 20)) * hypotenuse, dimensions.switchHoleSize / 2 - 5 + sin(.degrees(45 - 20)) * hypotenuse),
+            Vector2D(dimensions.thumbKeysXOffset + dimensions.switchHoleSize * 1.5 + dimensions.spacingBetweenSwitchHole + cos(.degrees(-45 - 20)) * hypotenuse, dimensions.switchHoleSize / 2 - 5 + sin(.degrees(-45 - 20)) * hypotenuse),
+            Vector2D(dimensions.thumbKeysXOffset + dimensions.switchHoleSize * 1.5 + dimensions.spacingBetweenSwitchHole + cos(.degrees(-135 - 20)) * hypotenuse, dimensions.switchHoleSize / 2 - 5 + sin(.degrees(-135 - 20)) * hypotenuse),
+            Vector2D(dimensions.thumbKeysXOffset + dimensions.switchHoleSize / 2 + cos(.degrees(-45 - 10)) * hypotenuse, dimensions.switchHoleSize / 2 + sin(.degrees(-45 - 10)) * hypotenuse),
+            Vector2D(dimensions.thumbKeysXOffset + dimensions.switchHoleSize / 2 + cos(.degrees(-135 - 10)) * hypotenuse, dimensions.switchHoleSize / 2 + sin(.degrees(-135 - 10)) * hypotenuse),
+            Vector2D(dimensions.pinkyMaxX, dimensions.pinkyMinY),
+            Vector2D(dimensions.pinkyMinX, dimensions.pinkyMinY)
         ])
         .filled()
-    }
-    
-    private func switchColumn(topLeft: Vector2D) -> any Geometry2D {
-        Union(
-            ([0, 1, 2] as [Double])
-                .map {
-                    switchShape(topLeft: Vector2D(topLeft.x, topLeft.y - (dimensions.switchHoleSize + dimensions.spacingBetweenSwitchHole) * $0))
-                }
-        )
-    }
-}
-
-extension FerrisSweep {
-    /// 0-indexed, left-most coordinate
-    // →┌────┐
-    //  |    |
-    //  └────┘
-    private func switchOffsetForRowAt(index: Int) -> Double {
-        (dimensions.switchHoleSize + dimensions.spacingBetweenSwitchHole) * Double(index)
-    }
-    
-    /// 0-indexed, top-most coordinate
-    //  ↓
-    //  ┌────┐
-    //  |    |
-    //  └────┘
-    private func switchOffsetForColumnAt(index: Int, startingOffset: Double) -> Double {
-        (dimensions.switchHoleSize + dimensions.spacingBetweenSwitchHole) * Double(index) + startingOffset
-    }
-    
-    private func switchOffsetFor(row: Int, column: Int, startingVerticalOffset: Double) -> Vector2D {
-        Vector2D(switchOffsetForRowAt(index: row), switchOffsetForColumnAt(index: column, startingOffset: startingVerticalOffset))
+        .offset(amount: 0.1, style: .round)
+        .stroked(width: 2, alignment: .outside, style: .round)
     }
 }
--- Sources/SwiftCad/Models/FingerOffsets.swift
+++ Sources/SwiftCad/Models/FingerOffsets.swift
@@ -6,6 +6,10 @@ struct FingerOffsets {
     var middle: Double
     var pointer: Double
     
+    var orderedOffsets: [Double] {
+        [pinky, ring, middle, pointer]
+    }
+    
     init(ring: Double = 20.5, middle: Double = 29.5, pointer: Double = 20.5) {
         self.ring = ring
         self.middle = middle
