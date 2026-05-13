28672b440c30b88dbf127b61eab419104246626b
Jordan C. <jordan.c4922@gmail.com>
2026-05-12 22:27:39 -0600

    Make keycap it's own type

--- Sources/SwiftCad/Main.swift
+++ Sources/SwiftCad/Main.swift
@@ -58,17 +58,7 @@ class Main {
             }
             
             await Model("modified_keycap") {
-                Import(model: URL(filePath: "SwiftCad_SwiftCad.bundle/Contents/Resources/src/1U_blank.3mf"))
-                    .rotated(.degrees(71.3), around: .x)
-                    .subtracting {
-                        let height = 4.995
-                        Circle(radius: 2.85)
-                            .stroked(width: 1, alignment: .outside, style: .round)
-                            .aligned(at: .center)
-                            .extruded(height: height)
-                            .translated(z: -height)
-                    }
-                    .rotated(.degrees(-71.3), around: .x)
+                ModifiedKeycap()
             }
         }
         
---
+++ Sources/SwiftCad/ModifiedKeycap.swift
@@ -0,0 +1,19 @@
+import Cadova
+import Foundation
+
+struct ModifiedKeycap: Shape3D {
+    private let rotation = Angle.degrees(71.3)
+    private let subtractionHeight = 4.995
+    var body: any Geometry3D {
+        Import(model: URL(filePath: "SwiftCad_SwiftCad.bundle/Contents/Resources/src/1U_blank.3mf"))
+            .rotated(rotation, around: .x)
+            .subtracting {
+                Circle(radius: 2.85)
+                    .stroked(width: 1, alignment: .outside, style: .round)
+                    .aligned(at: .center)
+                    .extruded(height: subtractionHeight)
+                    .translated(z: -subtractionHeight)
+            }
+            .rotated(-rotation, around: .x)
+    }
+}
